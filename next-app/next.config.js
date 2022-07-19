98c4a7cc892ebc6d3825a8d0b08ea0877a4e6926
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-07-20 03:57:22 +0700

    image

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['rickandmortyapi.com']
+    domains: ['rickandmortyapi.com'], 
+    loader: 'custom', 
+    path: '/'
   }
 }
 
